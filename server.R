9f7ed814710b5fb4d4665838dc5d63ca39aaf051
Kyle V Butler <kylevbutler@gmail.com>
2016-10-23 08:52:35 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,15 +1,13 @@
 library(shiny)
 library(readr)
+library(shinyIncubator)
 library(dplyr)
-library(ReactomePA)
-library(DOSE)
-library(reactome.db)
+library(tibble)
+#library(ReactomePA)
+#library(DOSE)
 library(igraph)
-library(graphite)
-library(GOSemSim)
-library(AnnotationDbi)
-library(png)
-library(grDevices)
+#library(png)
+#library(grDevices)
 probe_list <- read_csv("PROBELIST.csv")
 kegg_list <- read_csv("kegglist.csv")
 network_table <- read_csv("network_table.csv")
@@ -17,6 +15,7 @@ gene_names <- unique(probe_list$ENTREZ_GENE)
 gene_names <- as.character(gene_names)
 mygeneList <- rep(100, length(gene_names))
 names(mygeneList) <- gene_names
+probe_uniprot <- as.character(unique(probe_list$UNIPROTKB))
 
 shinyServer(function(input, output) {
   Data <- reactive({
@@ -25,34 +24,9 @@ shinyServer(function(input, output) {
       search_term <- trimws(input$search_in)
       search_out <- probe_list[apply(probe_list, 1, FUN = 
                                        function(x, y = search_term) {any(grepl(y, x, ignore.case = TRUE))}), ]
-      search_out <- search_out %>% dplyr::select(CHEMICAL_ID, TARGET_NAME, UNIPROTKB, DATA_SOURCE)
+      search_out <- search_out %>% dplyr::select(CHEMICAL_ID, TARGET_NAME, UNIPROTKB, DATA_SOURCE, is_agonist, orthogonal)
       if(search_term == "Search Input"){return()}
       return(search_out)
-    })
-  })
-  
-  Data_interact <- reactive({
-    input$goButton_interact
-    isolate({ 
-      search_term_interact <- trimws(input$search_in_interact)
-      search_out_interact <- network_table[apply(network_table[, 1:2], 1, FUN = 
-                                       function(x, y = search_term_interact) {any(grepl(y, x, ignore.case = TRUE))}), ]
-      if(search_term_interact == "Target UNIPROT ID"){return()}
-      return(search_out_interact)
-    })
-  })
-  
-  Data_interact_probes <- reactive({
-    input$goButton_interact
-    isolate({ 
-      search_term_interact <- trimws(input$search_in_interact)
-      search_out_interact <- network_table[apply(network_table[, 1:2], 1, FUN = 
-                                                   function(x, y = search_term_interact) {any(grepl(y, x, ignore.case = TRUE))}), ]
-      interacting_proteins <- c(search_out_interact$A, search_out_interact$B)
-      interacting_proteins_out <- probe_list %>% dplyr::filter(UNIPROTKB %in% interacting_proteins) %>% 
-        dplyr::select(CHEMICAL_ID, TARGET_NAME, UNIPROTKB, DATA_SOURCE)
-      if(search_term_interact == "Target UNIPROT ID"){return()}
-      return(interacting_proteins_out)
     })
   })
   
@@ -63,45 +37,127 @@ shinyServer(function(input, output) {
       filename <- normalizePath(file.path('./KeggPlots', kegg_list[kegg_list$EntryName == input$kegg_in, ]$filename))
       return(list(src = filename,
                   alt = paste("Image number", input$n)))
-      
     })
+    
   })
-  
-  reactome <- reactive({
-    input$goButton_reactome
-    isolate({ 
-      
-      return(tryCatch({
-        
-        viewPathway(trimws(input$reactome_in), organism = "human", readable=TRUE, fixed = TRUE,
-                    foldChange=mygeneList, vertex.label.cex = 0.75, col.bin = 1)
 
-      }, error=function(e){}))
-      
-    })
-  })
+  
+
+
   
   output$probePlot <- renderTable({
     Data()
   }, caption = "Probes found from basic search:",
   caption.placement = getOption("xtable.caption.placement", "top"))
-  
-  output$probePlot_interact <- renderTable({
-     Data_interact()
-   }, caption = "Protein interaction Search. Relevant protein-protein interactions:",
-  caption.placement = getOption("xtable.caption.placement", "top"))
+#---------------------------------------------------------------------------  
+  output$probePlot_interact <- renderPlot({
+    input$goButton_interact
+    isolate({
+    target <- trimws(input$search_in_interact)
+    network_table <- filter(network_table, confidenceScore >= input$slider1)
+    probe_uniprot <- as.character(unique(probe_list$UNIPROTKB))
+    target_connections <- network_table[network_table$A == target | network_table$B == target, ]
+    target_primary <- unique(c(target_connections$A, target_connections$B))
+    target_connections <- network_table[network_table$A %in% target_primary | network_table$B %in% target_primary, ]
+    relevant_probes <- probe_uniprot[probe_uniprot %in% unique(c(target_connections$A, target_connections$B))]
+    probe_primary <- network_table[network_table$A %in% relevant_probes | network_table$B %in% relevant_probes, ]
+    probe_primary <- unique(c(probe_primary$A, probe_primary$B))
+    middle_nodes <- probe_primary[probe_primary %in% target_primary]
+    #build attribute list
+    name_list <- tibble(node = c(target_connections$A, target_connections$B), alias = c(target_connections$aliasA, target_connections$aliasB))
+    name_list$char <- nchar(name_list$alias)
+    name_list <- name_list %>% group_by(node) %>% arrange(char) %>% dplyr::slice(1) %>% data.frame()
+    name_list$char[is.na(name_list$char)] <- 0
+    name_list$alias[name_list$char > 8] <- name_list$node[name_list$char > 8]
+    name_list$alias[name_list$char == 0] <- name_list$node[name_list$char == 0]
+    relevant_nodes <- tibble(node = unique(c(middle_nodes, target, relevant_probes)), node_type = "middle")
+    relevant_nodes <- left_join(relevant_nodes, name_list)
+    relevant_nodes[relevant_nodes$node == target, 2] <- "target"
+    relevant_nodes[relevant_nodes$node %in% relevant_probes, 2] <- "probe"
+    target_connections <- network_table[network_table$A %in% relevant_nodes$node & network_table$B %in% relevant_nodes$node, ]
+    g <- graph.data.frame(as.matrix(target_connections[, c(1, 2)]), vertices = relevant_nodes, directed = FALSE)
+    V(g)[V(g)$node_type == "probe"]$color <- "red"
+    V(g)[V(g)$node_type == "middle"]$color <- "yellow"
+    V(g)[V(g)$node_type == "target"]$color <- "lightblue"
+    V(g)$name <- V(g)$alias
+    E(g)$weight <- target_connections$confidenceScore * 7
+    plot(g, layout = layout.fruchterman.reingold, vertex.size = 4, vertex.label.cex = 1, vertex.label.dist=0.4, edge.curved = FALSE, 
+         edge.width=E(g)$weight)
+    })
+   }, height = 600, width = 900 )
    
-  output$probePlot_interact_probes <- renderTable({
-    Data_interact_probes()
-  }, caption = "Probes that may indirectly modulate the target:",
+  output$probePlot_interact_table <- renderTable({
+    input$goButton_interact
+    isolate({
+    target <- trimws(input$search_in_interact)
+    network_table <- filter(network_table, confidenceScore >= input$slider1)
+    probe_uniprot <- as.character(unique(probe_list$UNIPROTKB))
+    target_connections <- network_table[network_table$A == target | network_table$B == target, ]
+    target_primary <- unique(c(target_connections$A, target_connections$B))
+    target_connections <- network_table[network_table$A %in% target_primary | network_table$B %in% target_primary, ]
+    relevant_probes <- probe_uniprot[probe_uniprot %in% unique(c(target_connections$A, target_connections$B))]
+    interacting_proteins_out <- probe_list %>% dplyr::filter(UNIPROTKB %in% relevant_probes) %>% 
+      dplyr::select(CHEMICAL_ID, TARGET_NAME, UNIPROTKB, DATA_SOURCE)
+    print(interacting_proteins_out)
+    })
+
+  }, caption = "Probes in network:",
   caption.placement = getOption("xtable.caption.placement", "top"))
+
+  output$probePlot_interact_table_2 <- renderTable({
+    input$goButton_interact
+    isolate({
+      target <- trimws(input$search_in_interact)
+      network_table <- dplyr::filter(network_table, confidenceScore >= input$slider1)
+      probe_uniprot <- as.character(unique(probe_list$UNIPROTKB))
+      target_connections <- network_table[network_table$A == target | network_table$B == target, ]
+      target_primary <- base::unique(c(target_connections$A, target_connections$B))
+      target_connections <- network_table[network_table$A %in% target_primary | network_table$B %in% target_primary, ]
+      relevant_probes <- probe_uniprot[probe_uniprot %in% base::unique(c(target_connections$A, target_connections$B))]
+      probe_primary <- network_table[network_table$A %in% relevant_probes | network_table$B %in% relevant_probes, ]
+      probe_primary <- base::unique(c(probe_primary$A, probe_primary$B))
+      middle_nodes <- probe_primary[probe_primary %in% target_primary]
+      #build attribute list
+      name_list <- tibble(node = c(target_connections$A, target_connections$B), alias = c(target_connections$aliasA, target_connections$aliasB))
+      name_list$char <- nchar(name_list$alias)
+      name_list <- name_list %>% group_by(node) %>% arrange(char) %>% dplyr::slice(1) %>% data.frame()
+      name_list$char[is.na(name_list$char)] <- 0
+      name_list$alias[name_list$char > 8] <- name_list$node[name_list$char > 8]
+      name_list$alias[name_list$char == 0] <- name_list$node[name_list$char == 0]
+      relevant_nodes <- tibble(node = base::unique(c(middle_nodes, target, relevant_probes)), node_type = "middle")
+      relevant_nodes <- left_join(relevant_nodes, name_list)
+      relevant_nodes[relevant_nodes$node == target, 2] <- "target"
+      relevant_nodes[relevant_nodes$node %in% relevant_probes, 2] <- "probe"
+      target_connections <- network_table[network_table$A %in% relevant_nodes$node & network_table$B %in% relevant_nodes$node, ]
+      print(target_connections)
+    })
+  }, caption = "Relevant Interactions:",
+  caption.placement = getOption("xtable.caption.placement", "top"))  
+  
   
   output$reactomePlot <- renderPlot({
-    reactome()
-  })
+      input$goButton_reactome
+      isolate({ 
+        tryCatch({
+          print(viewPathway(trimws(input$reactome_in), organism = "human", readable=TRUE, fixed = TRUE,
+                            foldChange=mygeneList, vertex.label.cex = 0.75, col.bin = 1))
+        }, error=function(e){})
+      })
+  }, height = 500, width = 500)
   
   output$keggPlot <- renderImage({
     KEGG()
   }, deleteFile = FALSE)
+  
+  output$chembl_out <- renderUI({
+    input$goButtonChembl
+    isolate({
+      chembl_id <- trimws(input$chembl_in)
+      html_in <- '<object data="https://glados-ebitest.rhcloud.com/compound_report_card/CHEMBL25/embed/name_and_classification/" width="800px" height="350px"></object>'
+      html_in <- gsub("CHEMBL25", chembl_id, html_in, ignore.case = TRUE)
+      return(HTML(html_in))
+    #cat('<object data="https://glados-ebitest.rhcloud.com/compound_report_card/', html_in, '/embed/name_and_classification/" width="800px" height="350px"></object>', sep = "")
+      })
+  })
   
 })
--- ui.R
+++ ui.R
@@ -1,37 +1,40 @@
 
 
 library(shiny)
+library(shinyIncubator)
 library(readr)
 kegg_list <- read_csv("kegglist.csv")
+probe_list <- read_csv("PROBELIST.csv")
 shinyUI(navbarPage(
   "Chemical Probes App",
   tabPanel("Info",
            mainPanel(
-             strong("Information"),
-             div("Here you will find a comprehensive database of chemical probes. Compounds were classified as probes if they met the criteria defined 
+             h2("The chemical probes app"), br(),
+             img(src='golf.jpeg', align = "right", height = '200'), br(),
+             em("Now that you have biological data, how will you choose the best small molecule for pharmacological experiments?"), br(), br(),
+             div("This app will help life science researchers choose the best chemicals to modulate pathways and proteins of interest."),br(),br(),
+             div("This app contains a comprehensive database of chemical probes. 
+                Compounds were classified as probes if they met the criteria defined 
                by the ", a("Structural Genomics Consortium", href = "http://www.thesgc.org"), ("
                The properties of a probe, as defined by the SGC, are as follows:")), br(),
              p("1. In vitro potency of <100 nM at a single protein or two isoforms"),
              p("2. >30-fold selectivity vs other subfamilies"),
              p("3. Demonstration of on-target effect in cells at <1 μM"), br(),
              div("The Chembl database was analyzed to find probes that meet the above criteria. These probes were combined with user-validated probes 
-              from ", a("chemicalprobes.org", href = "http://www.chemicalprobes.org/"), (" to create a database. The database can be used in the 
+              from ", a("www.chemicalprobes.org", href = "http://www.chemicalprobes.org/"), (" to create the database. The database can be used in the 
                                                                                           following ways, accessible by the navigation bar:")), br(),
              strong("Chemical Probe Search"), br(),
-             em("Search for probes by protein target or by gene ontology id"), br(), br(),
-             em("Search by protein interaction networks"), br(),
+             p("Search for probes by chemical name, protein target, gene, kegg pathway id, reactome pathway id, or by gene ontology id"), br(),
+             strong("Search by protein interaction networks"), br(),
              p("Many proteins are not directly targeted by a chemical probe. Input the target of interest into 
-               the search bar, and the database will identify probes for any proteins that are known to directly interact with the target of interest. 
-               For example, searching for P04637 (TP53 uniprot id) will return BRD7 inhibitors, among others, as BRD7 is known to bind TP53. 
-               This function can also be used to find proteins that can be indirectly modulated with a chemical probe."),
-             p(""),
-             strong("Network Visualization"), br(),
-             em("Reactome network visualization"), br(),
-             p("View which nodes in reactome networks can be targeted by probes"),
-             em("KEGG network visualization"), br(),
+               the search bar, and the database will identify probes for any proteins in the local protein-protein interaction network. 
+               Interactions were extracted from the Intact database. "),
+             a("www.ebi.ac.uk/intact/", href = "http://www.ebi.ac.uk/intact/"), br(),
+             strong("KEGG network visualization"), br(),
              p("View which nodes in KEGG networks can be targeted by probes. Useful for generating ideas for combination therapies"), br(),
-             strong("Chembl compounds can be found at:"),
-             a("https://www.ebi.ac.uk/chembl/", href = "https://www.ebi.ac.uk/chembl/"), br()
+             p("This website was created with R shiny and uses hadleyverse and bioconductor packages. Kegg graphs were created 
+               with the pathview package."), br(),
+             p("Contact: kylevbutler@gmail.com")
              )),
   tabPanel(
     "Chemical Probe Search",
@@ -43,36 +46,52 @@ shinyUI(navbarPage(
           "Search for probes by target name, UNIPROT, or Gene ontology
           (e.g. GO:0070577 or 'lysine-acetylated histone binding')"
         ),
-        textInput("search_in", label = NULL, value = "Search Input"),
-        actionButton("goButton", "Search"), br(), br(),
-        strong("Protein interaction search"),
-        p(
-          "Search for probes by protein interaction network, using Uniprot IDs. Finds proteins that interact with the target, along with probes 
-          for those proteins."
+        textInput("search_in", label = NULL, value = "P42345"),
+        #selectInput("search_by", label = "Search By:", choices = 
+        #              names(probe_list)[c(1,2,3,8,11,12,13,14,15)]),
+        actionButton("goButton", "Search"),br(),br(),
+        strong("Display CHEMBL information"),
+        textInput("chembl_in", label = "Chembl ID", value = "CHEMBL1801204"),
+        actionButton("goButtonChembl", "Display", icon = icon("th"))
         ),
-        a("http://www.uniprot.org/", href = "http://www.uniprot.org/"),
-        textInput("search_in_interact", label = NULL, value = "Target UNIPROT ID"),
-        actionButton("goButton_interact", "Search")
-        ),
-      mainPanel(tableOutput("probePlot"), tableOutput("probePlot_interact_probes"), tableOutput("probePlot_interact"))
+        
+      mainPanel(tableOutput("probePlot"), htmlOutput("chembl_out"))
     )
   ),
   tabPanel(
-    "Network Visualization",
+    "Target Network Search",
+    sidebarLayout(
+      position = "left",
+      sidebarPanel(
+        strong("Target network search"),
+        p(
+          "Search for probes by protein target network, using Uniprot IDs. 
+          Finds proteins covered by probes in the local protein-protein interaction
+          network. Proteins associated with probes will 
+          be shown if they are separated from the target by one or two edges, and are  highlighted in red."
+        ), 
+        a("http://www.uniprot.org/", href = "http://www.uniprot.org/"),
+        textInput("search_in_interact", label = NULL, value = "P01116"),
+        p("Omit interactions below a given Intact confidence threshold to sparsify the graph."),
+        sliderInput("slider1", label = h3("Interaction confidence threshold"), min = 0.1, 
+                    max = 0.9, value = 0.36),
+        actionButton("goButton_interact", "Search")
+        
+        ),
+      mainPanel(plotOutput("probePlot_interact", width = "100%"), br(), br(), br(), br(), br(), br(), br(), tableOutput("probePlot_interact_table"),
+                tableOutput("probePlot_interact_table_2"))
+    )
+  ),
+  tabPanel(
+    "KEGG Pathway Visualization",
     sidebarLayout(
       sidebarPanel(
         position = "below",
-        strong("Visualize Reactome pathway"),
-        p(
-          "Enter a formal reactome pathway name, e.g. 'Regulation of TP53 Activity
-          through Methylation', or 'AKT phosphorylates targets in the cytosol'. Targets with
-          available probes appear in red.
-          Search terms must be punctuated exactly as they appear at:"
-        ),
-        a("www.reactome.org/PathwayBrowser/", href = "http://www.reactome.org/PathwayBrowser/"),
-        textInput("reactome_in", label = NULL, value = "Regulation of TP53 Activity through Methylation"),
-        actionButton("goButton_reactome", "Display"),
-        p(""),
+        # strong("Visualize Reactome pathway"),
+        # p("Enter Reactome pathway name exactly as it appears in Reactome database. e.g. 'Regulation of TP53 Activity through Methylation'"),
+        # a("reactome.org", href = "http://www.reactome.org/PathwayBrowser/"),
+        # textInput("reactome_in", label = NULL, value = "Regulation of TP53 Activity through Methylation"),
+        # actionButton("goButton_reactome", "Display"), br(), br(),
         strong("Visualize KEGG pathway"),
         p(
           "Select a KEGG pathway. Targets with available probes
@@ -82,7 +101,7 @@ shinyUI(navbarPage(
         selectInput("kegg_in", label = NULL, choices = kegg_list$EntryName),
         actionButton("goButton_kegg", "Display")
         ),
-      mainPanel(plotOutput("reactomePlot"), 
+      mainPanel(#plotOutput("reactomePlot",  width = "100%"), br(), br(), 
                 imageOutput("keggPlot")
                 )
       )
